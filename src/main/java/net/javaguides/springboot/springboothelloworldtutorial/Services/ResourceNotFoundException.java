ff8d16d8176d5a61a619f638ad85c558167f091e
Tzholdings <elvisuchechi@gmail.com>
2020-04-19 06:08:37 +0900

    complete springboot-tuts

--- src/main/java/net/javaguides/springboot/springboothelloworldtutorial/Services/ResourceNotFoundException.java
+++ src/main/java/net/javaguides/springboot/springboothelloworldtutorial/Services/ResourceNotFoundException.java
@@ -4,17 +4,21 @@ import org.apache.logging.log4j.util.Strings;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class ResourceNotFoundException extends Throwable {
-    public ResourceNotFoundException(Strings)
+    public ResourceNotFoundException(Strings);
+
     @ResponseStatus
     public class ResourceNotFoundException<L> extends RuntimeException {
 
-        private static final long serialVersionUID = 1 L;
+        private static final long serialVersionUID = 0x1;
+        L;
 
         public ResourceNotFoundException(String message) {
             super(message);
         }
 
         public ResourceNotFoundException(String message, Throwable throwable) {
+
             super(message, throwable);
         }
     }
+}
